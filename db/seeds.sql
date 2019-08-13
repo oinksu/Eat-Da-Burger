9af9495ba13d78a7e5f76b389b1b0c211ce69280
sinid <49916315+oinksu@users.noreply.github.com>
2019-08-13 18:38:02 -0400

    fixing issues

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,8 @@ INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
 INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
 INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
 INSERT INTO burgers (burger_name) VALUES ('Steak Burger');
+
+INSERT INTO burgers (burger_name) VALUES ('Wachito Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Tornado Burger');
